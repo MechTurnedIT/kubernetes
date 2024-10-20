603ca7081a7b82f8eff90dc2562954a9d4fb66ed
mechturnedit <scorpionking.mamlesh@gmail.com>
2024-10-20 22:05:54 +0530

    updates

--- day-4-horizonatl-scaling/test.sh
+++ day-4-horizonatl-scaling/test.sh
@@ -1,3 +1,6 @@
-for j in {1..10}; do  for i in {1..1000}; do
-    curl -s -o /dev/null -w "%{http_code}\n" http://a0fbf831ca4a94936954790ef7a89bc4-104519041.ap-south-1.elb.amazonaws.com/ &  done
-  waitdone
+for j in {1..10}; do
+  for i in {1..1000}; do
+    curl -s -o /dev/null -w "%{http_code}\n" http://a48a1d12b5d2e44cda4329315812a30e-1464521043.eu-west-1.elb.amazonaws.com/ &
+  done
+  wait  # This waits for all background jobs to finish before starting the next iteration of the outer loop
+done
